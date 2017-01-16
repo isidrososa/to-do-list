07fc46b4903701e778f9bc4618006651e7124bd4
Isidro Sosa <isidro.sosa.p@gmail.com>
2017-01-15 22:30:01 -0600

    Import monggose config in server.js

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 'use strict';
 
 var express = require('./config/lib/express'),
+    mongoose = require('./config/lib/mongoose'),
     port = process.env.PORT || 8080,
     chalk = require('chalk'),
     app = express();
+
+// Connect to database
+var db = mongoose();
 
 app.listen(port);
 
